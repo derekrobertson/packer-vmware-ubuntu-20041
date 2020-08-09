749e96035ab98ef10f1b697ff395a02e055f48e2
Derek Robertson <derek.robertson.uk@gmail.com>
2020-08-09 18:59:32 +0100

    ubuntu 20.04.1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # packer-vmware-ubuntu-20041
+<<<<<<< HEAD
 Packer scripts to produce ubuntu 20.04.1 image on VMware workstation
+=======
+Packer scripts to produce Ubuntu 20.04.1 image on VMware workstation
+
+    ./packer build .\ubuntu20041.json
+
+Tested with packer 1.6.1
+>>>>>>> d2dec8b... ubuntu 20.04.1
---
+++ curtin-clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Removes files leftover from the cloud-config install
+# If these are not removed, cloud-init cannot be run again (for example by Openstack)
+
+FILE=/etc/cloud/cloud.cfg.d/50-curtin-networking.cfg
+if test -f "$FILE"; then
+  sudo rm $FILE
+fi
+
+FILE=/etc/cloud/cloud.cfg.d/curtin-preserve-sources.cfg
+if test -f "$FILE"; then
+  sudo rm $FILE
+fi
+
+FILE=/etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg
+if test -f "$FILE"; then
+  sudo rm $FILE
+fi
